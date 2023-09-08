3a97648d8882d5b5d00ea5daecf4c2859114b500
Raphael Tarita <raphael.tarita@gmail.com>
2023-09-08 13:38:14 +0200

    add constructs for websocket communication

--- src/main/kotlin/com/rtarita/skull/common/CommonConstants.kt
+++ src/main/kotlin/com/rtarita/skull/common/CommonConstants.kt
@@ -5,9 +5,11 @@ import kotlinx.serialization.json.Json
 public object CommonConstants {
     public const val USER_ID_CLAIM: String = "userid"
     public const val WIN_WITH_POINTS: Int = 2
+    public const val WEBSOCKET_PING_PERIOD_SECONDS: Long = 10
+    public const val WEBSOCKET_TIMEOUT_SECONDS: Long = 30
+    public const val WEBSOCKET_MAX_FRAME_SIZE: Long = 128
+
     public val defaultHand: List<Card> = listOf(Card.ROSE, Card.ROSE, Card.ROSE, Card.SKULL)
-
-
     public val json: Json = Json {
         encodeDefaults = true
         isLenient = true
---
+++ src/main/kotlin/com/rtarita/skull/common/StateSignal.kt
@@ -0,0 +1,55 @@
+package com.rtarita.skull.common
+
+import java.util.Objects
+
+public sealed class StateSignal(
+    private val code: Byte,
+    public val gameid: String
+) {
+    public companion object {
+        private const val CODE_CLIENT_REQUEST_UPDATES: Byte = 0
+        private const val CODE_CLIENT_STOP_UPDATES: Byte = 1
+        private const val CODE_SERVER_ACK: Byte = 2
+        private const val CODE_SERVER_GAME_START: Byte = 3
+        private const val CODE_SERVER_GAME_UPDATE: Byte = 4
+        private const val CODE_SERVER_GAME_ENDED: Byte = 5
+
+        private fun unknownCode(code: Byte, domain: String? = null): Nothing =
+            throw IllegalArgumentException("unknown ${if (domain != null) "$domain " else ""}code: $code")
+
+        public fun deserialize(bytes: ByteArray): StateSignal {
+            val gameid = bytes.decodeToString(1)
+            return when (bytes.first()) {
+                CODE_CLIENT_REQUEST_UPDATES -> Client.RequestUpdates(gameid)
+                CODE_CLIENT_STOP_UPDATES -> Client.StopUpdates(gameid)
+                CODE_SERVER_ACK -> Server.Ack
+                CODE_SERVER_GAME_START -> Server.GameStart(gameid)
+                CODE_SERVER_GAME_UPDATE -> Server.GameUpdate(gameid)
+                CODE_SERVER_GAME_ENDED -> Server.GameEnded(gameid)
+                else -> unknownCode(bytes.first())
+            }
+        }
+
+        public fun deserializeClient(bytes: ByteArray): Client = deserialize(bytes) as? Client ?: unknownCode(bytes.first(), "client")
+
+        public fun deserializeServer(bytes: ByteArray): Server = deserialize(bytes) as? Server ?: unknownCode(bytes.first(), "server")
+    }
+
+    public fun serialize(): ByteArray = byteArrayOf(code) + gameid.encodeToByteArray()
+
+    public sealed class Client(code: Byte, gameid: String) : StateSignal(code, gameid) {
+        public class RequestUpdates(gameid: String) : Client(CODE_CLIENT_REQUEST_UPDATES, gameid)
+        public class StopUpdates(gameid: String) : Client(CODE_CLIENT_STOP_UPDATES, gameid)
+    }
+
+    public sealed class Server(code: Byte, gameid: String) : StateSignal(code, gameid) {
+        public object Ack : Server(CODE_SERVER_ACK, "")
+        public class GameStart(gameid: String) : Server(CODE_SERVER_GAME_START, gameid)
+        public class GameUpdate(gameid: String) : Server(CODE_SERVER_GAME_UPDATE, gameid)
+        public class GameEnded(gameid: String) : Server(CODE_SERVER_GAME_ENDED, gameid)
+    }
+
+    override fun equals(other: Any?): Boolean = if (other is StateSignal) code == other.code && gameid == other.gameid else false
+    override fun hashCode(): Int = Objects.hash(code, gameid)
+    override fun toString(): String = "$code#${this::class.simpleName}" + if (gameid.isNotEmpty()) "($gameid)" else ""
+}
