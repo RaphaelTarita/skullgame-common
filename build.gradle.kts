0f1f2620544c5ab483d907d7fd5fef866070de26
Raphael Tarita <raphael.tarita@gmail.com>
2023-09-08 17:16:48 +0200

    version upgrades: kotlin: 1.9.0 -> 1.9.10 kotlinx-serialization: 1.9.0 -> 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
     `java-library`
     `maven-publish`
