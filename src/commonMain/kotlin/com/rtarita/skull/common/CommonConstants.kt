f3c2e0b48278339b42585ca0f92b8c6f2c332b20
Raphael Tarita <raphael.tarita@gmail.com>
2024-10-23 15:16:06 +0200

    change WEBSOCKET_PING_PERIOD_SECONDS and WEBSOCKET_TIMEOUT_SECONDS to Duration (due to upgrade to Ktor 3.0.0)

--- src/commonMain/kotlin/com/rtarita/skull/common/CommonConstants.kt
+++ src/commonMain/kotlin/com/rtarita/skull/common/CommonConstants.kt
@@ -2,13 +2,15 @@ package com.rtarita.skull.common
 
 import com.rtarita.skull.common.state.Card
 import kotlinx.serialization.json.Json
+import kotlin.time.Duration
 
 public object CommonConstants {
     public const val USER_ID_CLAIM: String = "userid"
     public const val WIN_WITH_POINTS: Int = 2
-    public const val WEBSOCKET_PING_PERIOD_SECONDS: Long = 10
-    public const val WEBSOCKET_TIMEOUT_SECONDS: Long = 30
     public const val WEBSOCKET_MAX_FRAME_SIZE: Long = 128
+
+    public val WEBSOCKET_PING_PERIOD: Duration = with(Duration) { 10.seconds }
+    public val WEBSOCKET_TIMEOUT: Duration = with(Duration) { 30.seconds }
 
     public val defaultHand: List<Card> = listOf(Card.ROSE, Card.ROSE, Card.ROSE, Card.SKULL)
     public val json: Json = Json {
